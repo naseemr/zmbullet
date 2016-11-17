42cb4e56c29778e93304cace14c7d958b239a99d
naseemr <naseemra@gmail.com>
2016-11-17 17:48:47 -0600

    updated with minor change

--- zbbullet.pl
+++ zbbullet.pl
@@ -1,30 +1,20 @@
-#!/usr/bin/perl -T
+#!/usr/bin/perl -w
 
-use File::Basename;
-use Data::Dumper;
-use bytes;
 use strict;
 use WWW::PushBullet;
-use lib '/usr/local/lib/x86_64-linux-gnu/perl5';
-use POSIX;
 use ZoneMinder;
-use warnings;
 use DBI;
+
 
 $| = 1;
 
+#zmDbgInit( "myscript", level=>0, to_log=>0, to_syslog=>0, to_term=>1 );
 
-my $driver = "mysql";
-my $database = "zm";
-my $user = "admin";
-my $password = "my1Sadia";
-
-
+#my $dbh = DBI->connect( "DBI:mysql:database=".ZM_DB_NAME.";host=".ZM_DB_HOST, ZM_DB_USER, ZM_DB_PASS );
 my $dbh = zmDbConnect();
-my $sql = "select M.*, max(E.Id) as LastEventId from Monitors as M left join Events as E on M.Id = E.MonitorId where M.Function != 'None' group by (M.Id)";
 
-my $sth = $dbh->prepare_cached( $sql )
-    or Fatal( "Can't prepare '$sql': ".$dbh->errstr() );
+my $sql = "select M.*, max(E.Id) as LastEventId from Monitors as M left join Events as E on M.Id = E.MonitorId where M.Function != 'None' group by (M.Id)";
+my $sth = $dbh->prepare_cached( $sql ) or die( "Can't prepare '$sql': ".$dbh->errstr() );
 
 my $res = $sth->execute() or die( "Can't execute '$sql': ".$sth->errstr() );
 my @monitors;
@@ -43,9 +33,9 @@ while( 1 )
         {
             $monitor->{LastEventId} = $last_event_id;
             
-            my $pb = WWW::PushBullet->new({apikey => 'your pushbullet api key'});
-             $pb->push_link({ device_id => 'all', title => 'Motion event '.$last_event_id,url => 'http://<your domain>/zm/index.php?view=event&eid='.$last_event_id });
-            while (zmInAlarm($monitor)) { };
+             my $pb = WWW::PushBullet->new({apikey => 'your pushbullet api key'});
+              $pb->push_link({ device_id => 'all', title => 'Motion event '.$last_event_id,url => '<your zm url>/zm/index.php?view=event&eid='.$last_event_id });
+             while (zmInAlarm($monitor)) { };
         }
     }
     sleep( 1 );
